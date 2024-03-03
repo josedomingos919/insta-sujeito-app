21a0d03aa121c2dda559df36796d82ff62735c4b
josedomingos919 <josedomingos919@gmail.com>
2024-03-03 16:54:23 +0100

    feat: create app component

--- App.js
+++ App.js
@@ -1,20 +1,117 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Component } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TouchableOpacity,
+  FlatList,
+} from "react-native";
+import List from "./src/components/list";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      feed: [
+        {
+          id: "1",
+          nome: "Lucas Silva",
+          descricao: "Mais um dia de muitos bugs :)",
+          imgperfil:
+            "https://sujeitoprogramador.com/instareact/fotoPerfil1.png",
+          imgPublicacao: "https://sujeitoprogramador.com/instareact/foto1.png",
+          likeada: false,
+          likers: 0,
+        },
+        {
+          id: "2",
+          nome: "Matheus",
+          descricao: "Isso sim é ser raiz!!!!!",
+          imgperfil:
+            "https://sujeitoprogramador.com/instareact/fotoPerfil2.png",
+          imgPublicacao: "https://sujeitoprogramador.com/instareact/foto2.png",
+          likeada: false,
+          likers: 0,
+        },
+        {
+          id: "3",
+          nome: "Jose Augusto",
+          descricao: "Bora trabalhar Haha",
+          imgperfil:
+            "https://sujeitoprogramador.com/instareact/fotoPerfil3.png",
+          imgPublicacao: "https://sujeitoprogramador.com/instareact/foto3.png",
+          likeada: false,
+          likers: 3,
+        },
+        {
+          id: "4",
+          nome: "Gustavo Henrique",
+          descricao: "Isso sim que é TI!",
+          imgperfil:
+            "https://sujeitoprogramador.com/instareact/fotoPerfil1.png",
+          imgPublicacao: "https://sujeitoprogramador.com/instareact/foto4.png",
+          likeada: false,
+          likers: 1,
+        },
+        {
+          id: "5",
+          nome: "Guilherme",
+          descricao: "Boa tarde galera do insta...",
+          imgperfil:
+            "https://sujeitoprogramador.com/instareact/fotoPerfil2.png",
+          imgPublicacao: "https://sujeitoprogramador.com/instareact/foto5.png",
+          likeada: false,
+          likers: 32,
+        },
+      ],
+    };
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.header}>
+          <TouchableOpacity activeOpacity={0.1}>
+            <Image style={styles.logo} source={require("./src/img/logo.png")} />
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <Image style={styles.send} source={require("./src/img/send.png")} />
+          </TouchableOpacity>
+        </View>
+
+        <FlatList
+          showsVerticalScrollIndicator={false}
+          data={this.state.feed}
+          renderItem={({ item }) => <List data={item} />}
+        />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
+    marginTop: 20,
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  header: {
+    padding: 5,
+    height: 55,
+    backgroundColor: "#fff",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+
+    borderBottomWidth: 0.2,
+    borderColor: "rgba(0,0,0,0.1)",
+    shadowColor: "#000",
+    elevation: 1,
+  },
+  logo: {},
+  send: {
+    width: 23,
+    height: 23,
   },
 });
